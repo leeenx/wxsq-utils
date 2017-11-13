715c83518127f25dd3e3089ea7b2cb84f43b6100
leeenx <leeenx@users.noreply.github.com>
2017-11-13 01:12:45 -0600

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+/*
+  @ author: leeenx
+  @ 微信手Q常用js
+*/
+
+// 封装微信手Q的 visibilitychange 事件
+document.addEventListener("qbrowserVisibilityChange", function(e){
+    var evt = document.createEvent("HTMLEvents"); 
+    evt.initEvent("visibilitychange", false, false); 
+    evt.hidden = e.hidden; 
+    document.dispatchEvent(evt); 
+}, true); 
+document.addEventListener("visibilitychange", function(e) {
+    e.hidden = e.hidden === undefined ? document.hidden : e.hidden; 
+}, true); 
